6b9a817654371608f06a93ab0ab6dfe94590fbf7
Carlos Silva <148013422+PumpKinSuicidal@users.noreply.github.com>
2025-01-27 17:02:44 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+/**
+* Simulador dos operadores logicos 
+*/
+let sw1 = false
+let sw2 = false
+let quebrado = 0
+function sw(receive) {
+    // Cria um objeto usando a classe Audio
+    if(receive === 3) {
+    let som = new(Audio)
+    som.src = "sound/glassbreaking.wav"
+    som.play()
+    // Trocar a imagem
+    quebrado = 1
+    } 
+    if(quebrado == 1) {
+        document.getElementById('lamp').src = "img/broken.jpg"
+    }
+    if(receive === 1 && sw1 == false){
+        document.getElementById('sw1').src = "img/swon.png"
+        sw1 = true
+    } else if(receive === 1 && sw1 == true) {
+        document.getElementById('sw1').src = "img/swoff.png"
+        sw1 = false
+    }
+
+      if(receive === 2 && sw2 === false){
+        document.getElementById('sw2').src = "img/swon.png"
+        sw2 = true
+    } else if(receive === 2 && sw2 === true) {
+        document.getElementById('sw2').src = "img/swoff.png"
+        sw2 = false
+    }
+// Ou, colocar um if pra executar o codigo da lampada
+    if(sw1 === true && sw2 === true && quebrado === 0) {
+        document.getElementById('lamp').src = "img/on.jpg"
+    } else if (sw1 === false && quebrado !== 1 || sw2 === false && quebrado !== 1) {
+        document.getElementById('lamp').src = "img/off.jpg"
+    }
+
+}
